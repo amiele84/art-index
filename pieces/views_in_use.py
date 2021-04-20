d878089107e5701155c708beb72728d39ce5e886
amiele84 <amiele84@gmail.com>
2021-04-20 00:13:36 -0400

    test2

--- pieces/views_in_use.py
+++ pieces/views_in_use.py
@@ -1,3 +1,94 @@
+from django.shortcuts import render
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse_lazy
+
+from django.views import generic
+from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django_filters.views import FilterView
+from django_tables2.views import SingleTableMixin
+
+from django_tables2 import SingleTableView
+
+from django.utils.decorators import method_decorator
+
+from .forms import *
+from .tables import *
+from .filters import *
+from .models import *
+
+# Create your views here.
+
+##sidebar
+def index(request):
+    return render(request, 'index.html')
+
+
+                ## pieces ##
+
+##3 types of views for pieces lists
+
+#Large:title, img large thumbnail w/ 3x3
+class PiecesListLargeView(SingleTableView, FilterView):
+    pass
+
+
+##in use 
+
+#Medium:
+#single col
+class PieceListView(LoginRequiredMixin, FilterView):
+    table_class = PiecesTableMedium
+    model = NewPiece
+    template_name = "pieces/pieces_list_working_copy.html"
+    #template_name = "pieces/pieces_try_in_use.html"    #up-to-date
+    context_object_name = "pieces"
+#    ordering = ["location"]
+   # paginate_by = 5
+
+    filterset_class = FilterTags
+
+    def get_ordering_location(self):
+        sort = self.request.GET.get('sort','location') #Order live feed events according to closest start date events at the top
+        return sort
+    
+
+#class FilteredPieceListView2(SingleTableMixin, FilterView):
+#    table_class = PiecesTableMedium
+#    model = NewPiece
+#    template_name = "pieces/pieces.html"
+
+    #filterset_class = AvailabilityFilter
+
+#Detail:
+#single column 
+class PieceDetailView(generic.DetailView):
+    model = NewPiece
+    template_name = 'pieces/piece_detail_working_copy.html'
+    context_object_name = 'piece'
+
+
+#update piece
+#directs from collections url
+#just can't change img
+class PieceUpdateView(generic.UpdateView):
+    model = NewPiece 
+    fields = [
+            'artist', 'title',
+            'description', 'types', 'medium',
+            'subject_matter', 'location', 'date_of_upload',
+            'visibility', 'width', 'height', 'depth', 'dim',
+            'availability', 'price','tags',
+            ]
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('artwork:pieces')
+
 #delete piece
 class PieceDeleteView(DeleteView):
     model = NewPiece
@@ -17,7 +108,7 @@ class PieceDeleteView(DeleteView):
 #creates new Painting Model
 class NewPieceUploadView(LoginRequiredMixin, TemplateView):
     form = NewUploadForm
-    template_name = 'pieces/new_upload2_crispy_working_copy.html'
+    template_name = 'pieces/add_piece_working_copy.html'
     #template_name = 'pieces/new_upload2_crispy_in_use.html'    #has dropbox
 
     def post(self, request, *args, **kwargs):
@@ -33,6 +124,25 @@ class NewPieceUploadView(LoginRequiredMixin, TemplateView):
 
     def get(self, request, *args, **kwargs):
         return self.post(request, *args, **kwargs)
+        
+
+
+
+##collections
+class CollectionsListView(LoginRequiredMixin, generic.ListView):
+    model = NewPiece
+    queryset = NewPiece.objects.filter(location='NYC')
+    template_name ='pieces/collections_working_copy.html'
+    #paginate_by = 10
+    context_object_name = 'collections'
+
+    filterset = CollectionFilter
+
+#    def get_queryset(self):
+#        collection = NewPiece.objects.filter(uploader_id=self.request.user).values()
+#        return render( 'pieces/collections_working_copy.html', {'collection': collection})
+#        return NewPiece.objects.filter(uploader=self.request.user)
+
 
 
 ##end of in use##
